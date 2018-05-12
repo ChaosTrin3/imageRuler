9ebbbe7c04fae5b189c2e10c4479b2c425b4362d
JoshT <jdt015@bucknell.edu>
2018-05-12 17:00:55 -0400

    I just added the correct .xml datasts from the OpenCV download so I have access to them anywhere :)

--- cannyEdgeDetector.py
+++ cannyEdgeDetector.py
@@ -7,6 +7,10 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+#pathToHaarCascades = 'C:/Users/jdt01/Downloads/opencv/sources/data/haarcascades/'
+# The path to the required datasets are added to the repo specifically..
+pathToHaarCascades = ''
+
 #while True:
 lowLim = int(input("lowLim of the Canny hysteresis input: "))
 highLim = int(input("highLim of the Canny hysteresis input: "))
@@ -20,3 +24,25 @@ plt.subplot(122),plt.imshow(edges,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
 plt.show()
+
+# Object recognition, although it is using a standard face database...
+# The core tech will be used to find pockets relative to edges..
+
+face_cascade = cv2.CascadeClassifier(pathToHaarCascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(pathToHaarCascades + 'haarcascade_eye.xml')
+
+img = cv2.imread('face.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
