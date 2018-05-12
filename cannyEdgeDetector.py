2f1e210a784751153e5e9b293961beb850b695c9
JoshT <jdt015@bucknell.edu>
2018-05-12 12:27:21 -0400

    This is the first commit of the cv2.Canny() implementation.

---
+++ cannyEdgeDetector.py
@@ -0,0 +1,21 @@
+## Exploration of the edge detection function Canny() which uses the Canny algorithm to determine edges within an image.
+# This is a cv2.Canny() example from:
+# http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_canny/py_canny.html
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+while True:
+    lowLim = int(input("lowLim of the Canny hysteresis input: "))
+    highLim = int(input("highLim of the Canny hysteresis input: "))
+
+    img = cv2.imread('dahlia.jpg',0)
+    edges = cv2.Canny(img,lowLim,highLim)
+
+    plt.subplot(121),plt.imshow(img,cmap = 'gray')
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+    plt.show()
